10ca77b062cd8ad539a44897b3aa0ddc258ebefd
wdyntr <widyantoro0603@gmail.com>
2024-07-20 13:25:03 +0700

    revisi dashboard

--- datatel/resources/views/dashboard.blade.php
+++ datatel/resources/views/dashboard.blade.php
@@ -3,27 +3,25 @@
 @section('title', 'Dashboard - Data Pelanggan')
 
 @section('contents')
-<div class="row">
-    <div class="row">
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4" >
-            <div class="card border-left-primary shadow h-70 py-2">
-                <a href="{{ route('dataPelanggan') }}">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Data Pelanggan</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
+<div class="row" style="border:2px solid red;">
+
+                        <!-- Earnings (Monthly) Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4" >
+                            <div class="card border-left-primary shadow h-70 py-2">
+                                <a href="{{ route('dataPelanggan') }}">
+                                    <div class="card-body">
+                                        <div class="row no-gutters align-items-center">
+                                            <div class="col mr-2">
+                                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                    Data Pelanggan</div>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
                         </div>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </div>
 </div>
 @endsection
