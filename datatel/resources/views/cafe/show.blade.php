4e228c74a2426ed4f86ba0a9f8ee64cf751672df
Ahmad Zain Mahmud <ahmadzayn010@gmail.com>
2024-07-25 16:30:24 +0700

    add edit (cafe)

--- datatel/resources/views/cafe/show.blade.php
+++ datatel/resources/views/cafe/show.blade.php
@@ -5,53 +5,43 @@
 <hr />
 <div class="row">
     <div class="col mb-3">
-        <label class="form-label">Nama Universitas</label>
-        <input type="text" name="nama_univ" class="form-control" placeholder="Nama Universitas"
-            value="{{ $univ->nama_univ }}" readonly>
+        <label class="form-label">Nama</label>
+        <input type="text" name="nama" class="form-control" placeholder="Nama" value="{{ $cafe->nama }}" readonly>
     </div>
     <div class="col mb-3">
-        <label class="form-label">Status</label>
-        <input type="text" name="status" class="form-control" placeholder="Status" value="{{ $univ->status }}" readonly>
+        <label class="form-label">Lokasi</label>
+        <input type="text" name="lokasi" class="form-control" placeholder="Lokasi" value="{{ $cafe->lokasi }}" readonly>
     </div>
 </div>
 <div class="row">
     <div class="col mb-3">
-        <label class="form-label">Alamat</label>
-        <input name="alamat" class="form-control" placeholder="Alamat" value="{{ $univ->alamat }}" readonly>
+        <label class="form-label">Sudah Berlangganan/Belum</label>
+        <input name="berlangganan" class="form-control" placeholder="Sudah Berlangganan/Belum"
+            value="{{ $cafe->berlangganan }}" readonly>
     </div>
     <div class="col mb-3">
-        <label class="form-label">Kecamatan</label>
-        <input type="text" name="kecamatan" class="form-control" placeholder="Kecamatan" value="{{ $univ->kecamatan }}"
+        <label class="form-label">Email/Sosmed</label>
+        <input type="text" name="email_sosmed" class="form-control" placeholder="Email/Sosmed"
+            value="{{ $cafe->email_sosmed }}" readonly>
+    </div>
+</div>
+<div class="row">
+    <div class="col mb-3">
+        <label class="form-label">Jenis Layanan</label>
+        <input class="form-control" name="jenis_layanan" placeholder="jenis_layanan" value="{{ $cafe->jenis_layanan }}"
             readonly>
-    </div>
-</div>
-<div class="row">
-    <div class="col mb-3">
-        <label class="form-label">Kabupaten</label>
-        <input class="form-control" name="kabupaten" placeholder="Kabupaten" value="{{ $univ->kabupaten }}" readonly>
-    </div>
-    <div class="col mb-3">
-        <label class="form-label">AM</label>
-        <input type="text" name="am" class="form-control" placeholder="AM" value="{{ $univ->am }}" readonly>
-    </div>
-</div>
-<div class="row">
-    <div class="col mb-3">
-        <label class="form-label">Layanan Telkom</label>
-        <input class="form-control" name="layanan_telkom" placeholder="layanan_telkom"
-            value="{{ $univ->layanan_telkom }}" readonly>
     </div>
 </div>
 <div class="row">
     <div class="col mb-3">
         <label class="form-label">Created At</label>
         <input type="text" name="created_at" class="form-control" placeholder="Created At"
-            value="{{ $univ->created_at }}" readonly>
+            value="{{ $cafe->created_at }}" readonly>
     </div>
     <div class="col mb-3">
         <label class="form-label">Updated At</label>
         <input type="text" name="updated_at" class="form-control" placeholder="Updated At"
-            value="{{ $univ->updated_at }}" readonly>
+            value="{{ $cafe->updated_at }}" readonly>
     </div>
 </div>
 @endsection
