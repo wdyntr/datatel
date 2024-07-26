6f250fb48628c7eb0a7e0aa4bb086cd4077649fc
Ahmad Zain Mahmud <ahmadzayn010@gmail.com>
2024-07-26 14:05:38 +0700

    add edit delete cafe

--- datatel/resources/views/cafe/edit.blade.php
+++ datatel/resources/views/cafe/edit.blade.php
@@ -3,56 +3,44 @@
 @section('contents')
 <h1 class="mb-0">Edit Data</h1>
 <hr />
-<form action="{{ route('univ.update', $univ->id) }}" method="POST">
+<form action="{{ route('cafe.update', $cafe->id) }}" method="POST">
     @csrf
     @method('PUT')
     <div class="row">
         <div class="col mb-3">
-            <label class="form-label">Nama Universitas</label>
-            <input type="text" name="nama_univ" class="form-control" placeholder="Nama Universitas/Sekolah Tinggi"
-                value="{{ $univ->nama_univ }}">
+            <label class="form-label">Nama</label>
+            <input type="text" name="nama" class="form-control" placeholder="Nama" value="{{ $cafe->nama }}">
         </div>
         <div class="col mb-3">
-            <label class="form-label">Status</label>
-            <select class="form-control" name="status">
-                <option value="">-- Pilih Ketersediaan Jaringan Optik --</option>
-                <option value="Negeri" {{ $univ->status == 'Negeri' ? 'selected' : '' }}>Negeri</option>
-                <option value="Swasta" {{ $univ->status == 'Swasta' ? 'selected' : '' }}>Swasta</option>
+            <label class="form-label">Lokasi</label>
+            <input type="text" name="lokasi" class="form-control" placeholder="Lokasi" value="{{ $cafe->lokasi }}">
+        </div>
+    </div>
+    <div class="row">
+
+        <div class="col mb-3">
+            <label class="form-label">Status Berlangganan</label>
+            <select class="form-control" name="berlangganan">
+                <option value="">-- Sudah Berlanggaan/Belum --</option>
+                <option value="Sudah" {{ $cafe->berlangganan == 'Sudah' ? 'selected' : '' }}>Sudah</option>
+                <option value="Belum" {{ $cafe->berlangganan == 'Belum' ? 'selected' : '' }}>Belum</option>
             </select>
         </div>
-    </div>
-    <div class="row">
+
         <div class="col mb-3">
-            <label class="form-label">Alamat</label>
-            <input name="alamat" class="form-control" placeholder="Alamat" value="{{ $univ->alamat }}">
-        </div>
-        <div class="col mb-3">
-            <label class="form-label">Kecamatan</label>
-            <input type="text" class="form-control" name="kecamatan" placeholder="Kecamatan"
-                value="{{ $univ->kecamatan }}">
+            <label class="form-label">Email/Sosmed</label>
+            <input type="text" class="form-control" name="email_sosmed" placeholder="email_sosmed"
+                value="{{ $cafe->email_sosmed }}">
         </div>
     </div>
     <div class="row">
         <div class="col mb-3">
-            <label class="form-label">Kabupaten</label>
-            <input type="text" name="kabupaten" class="form-control" placeholder="Kabpaten"
-                value="{{ $univ->kecamatan }}">
-        </div>
-        <div class="col mb-3">
-            <label class="form-label">AM</label>
-            <input class="form-control" name="am" placeholder="AM" value="{{ $univ->am }}">
+            <label class="form-label">Jenis Layanan</label>
+            <input type="text" name="jenis_layanan" class="form-control" placeholder="Jenis Layanan"
+                value="{{ $cafe->jenis_layanan }}">
         </div>
     </div>
-    <div class="row">
-        <div class="col mb-3">
-            <label class="form-label">Layanan Telkom</label>
-            <select class="form-control" name="layanan_telkom">
-                <option value="">-- Pilih Ketersediaan Jaringan Optik --</option>
-                <option value="Sudah" {{ $univ->layanan_telkom == 'Sudah' ? 'selected' : '' }}>Sudah</option>
-                <option value="Belum" {{ $univ->layanan_telkom == 'Belum' ? 'selected' : '' }}>Belum</option>
-            </select>
-        </div>
-    </div>
+
     <div class="row">
         <div class="d-grid">
             <button class="btn btn-warning">Update</button>
